98ac007556fdf8fb415fbee1c1e9f81a73cee847
wuyeelok <wuyeelok@gmail.com>
2024-06-07 22:12:07 +0800

    Quick test run on httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private userService: UserService = inject(UserService);
+
   title = 'Learn HTTP Observable';
+
+  ngOnInit(): void {
+    this.userService.checkCurrentWeather();
+  }
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,26 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient) {}
+  private http: HttpClient = inject(HttpClient);
+
+  checkCurrentWeather() {
+    this.http
+      .get('https://data.weather.gov.hk/weatherAPI/opendata/weather.php', {
+        params: {
+          dataType: 'rhrread',
+          lang: 'en',
+        },
+      })
+      .subscribe({
+        next: (data) => alert(`Current weather: ${Object.keys(data)}`),
+        error: (err) => console.error('something is wrong: ' + err),
+        complete: () => {
+          console.log('finished fetching current weather');
+        },
+      });
+  }
 }
