fd2c397f1db17a538abb44ed5f9ac47be217f30f
wuyeelok <wuyeelok@gmail.com>
2024-06-09 10:08:57 +0800

    Quick overview of observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { UserService } from './service/user.service';
+import { Observable, Subscription } from 'rxjs';
+import { DumbHttpRespone } from './interface/dumb-http-respone';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,37 @@ export class AppComponent implements OnInit {
 
   title = 'Learn HTTP Observable';
 
+  private dummySubscription: Subscription = new Subscription();
+
+  private dummyObserva$ = new Observable<DumbHttpRespone>((sub) => {
+    console.log('Inside sub');
+    sub.next({ code: 200, data: 'this is data 1' });
+    sub.next({ code: 200, data: 'this is data 2' });
+
+    setTimeout(() => {
+      sub.next({ code: 200, data: 'this is data 3' });
+    }, 1000);
+
+    setTimeout(() => {
+      sub.error({ code: 500, msg: 'Error occurred' });
+    }, 1300);
+
+    setTimeout(() => {
+      sub.complete();
+    }, 2000);
+    console.log('finished emitted');
+  });
+
   ngOnInit(): void {
-    // this.userService.checkCurrentWeather();
+    const dummyObserver = {
+      next: (res: DumbHttpRespone) => {
+        console.log('got response: ', res.code, res.data);
+      },
+      error: (err: any) => {
+        console.error(`What's wrong: ${err}`);
+      },
+      complete: () => console.log('Done!!!@!!!'),
+    };
+    this.dummySubscription = this.dummyObserva$.subscribe(dummyObserver);
   }
 }
---
+++ src/app/interface/dumb-http-respone.ts
@@ -0,0 +1,4 @@
+export interface DumbHttpRespone {
+  code: number;
+  data: string;
+}
